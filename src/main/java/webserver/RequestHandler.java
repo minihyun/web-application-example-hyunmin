e6e7277114b128c2c15fb7a5b1de6dad887335c6
Lee HyunMin <hm.lee@campmobile.com>
2015-02-13 14:13:59 +0900

    요구사항 2 해결 (queryString 처리 및 User 정보 저장)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,9 +9,14 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
+
+import model.User;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -32,14 +37,25 @@ public class RequestHandler extends Thread {
 			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
 			String line = "", uri = "", httpVersion = "", acceptType= "", host = "";
 			String[] tokens;
+			String requestPath = "", params = "";
 			
 			// Get First Line Information
 			if(!"".equals(line = br.readLine())) {
 				tokens = line.split(" ");
 				if(tokens[0].equals("GET")) {
-					uri = tokens[1];
 					httpVersion = tokens[2];
+					
+					uri = tokens[1];
+					int index = uri.indexOf("?");
+					requestPath = uri.substring(0, index);
+					params = uri.substring(index+1);
 				}
+			}
+			
+			// TODO 나중에 POST에 넣을 것
+			if(requestPath.equals("/create")) {
+				Map<String, String> paramsMap = HttpRequestUtils.parseQueryString(params);
+				User userInfo = new User(paramsMap.get("userId"), paramsMap.get("password"), paramsMap.get("name"), paramsMap.get("email"));
 			}
 			
 			while (!"".equals(line = br.readLine())) {
@@ -53,7 +69,7 @@ public class RequestHandler extends Thread {
 				}
 			}
 			
-			byte[] body = Files.readAllBytes(new File("./webapp" + uri).toPath());
+			byte[] body = Files.readAllBytes(new File("./webapp" + requestPath).toPath());
 
 			DataOutputStream dos = new DataOutputStream(out);
 			response200Header(dos, httpVersion, body.length);
